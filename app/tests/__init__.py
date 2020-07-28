5b1d4b3748df7d0938c3362e612e7262aaac136e
liuhuan <liuhuan@thinktrader.net>
2020-07-28 16:16:55 +0800

    add

---
+++ app/tests/__init__.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+"""
+@Time       :2020/7/28 16:01
+@Author     :liuhuan
+@verssion   :v1.0
+@effect     :TODO
+"""
---
+++ config.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+"""
+@Time       :2020/7/28 16:02
+@Author     :liuhuan
+@verssion   :v1.0
+@effect     :配置文件
+"""
+
+import os
+basedir = os.path.abspath(os.path.dirname(__file__))
+
